43eeebf30c2c625f85715900580bf69631b626ac
Chathushi Jayarathna <124165734+Chathu-Jayarathna@users.noreply.github.com>
2024-12-17 21:28:55 +0530

    fixed hero

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,17 +1,10 @@
 import React from 'react'
 import { Spotlight } from "../Components/ui/Spotlight";
-import { TextGenerateEffect } from "../Components/ui/text-generate-effect";
-import { FlipWords } from "../Components/ui/FlipWords";
-
-
-
-
-const heading = ` Turning ideas into intuitive digital solutions`;
+import { TextGenerateEffect } from "../Components/ui/TextGenerateEffect";
+import { FaLocationArrow } from "react-icons/fa6";
+import MagicButton from "../Components/ui/MagicButton";
 
 const Home = () => {
-
-  const flipewords = ["Frontend Developer", "Full-Stack Developer", "Backend Developer", "Web Developer"];
-  
 
   return (
     <div id='home'>
@@ -19,19 +12,31 @@ const Home = () => {
         className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center antialiased bg-grid-white/[0.02] relative overflow-hidden">
         <Spotlight className="-top-2 -left-20 md:-left-12 md:-top-20" fill="white" />
         <Spotlight className="-top-12 -left-20 md:left-60 md:-top-20" fill="blue" />
-        <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-44 md:pt-0">
-          <h1
-            className="text-4xl tracking-wider md:text-6xl uppercase font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
-            <TextGenerateEffect words={heading} />
-          </h1>
-          <h3
-            className="text-lg md:text-2xl tracking-wider font-medium text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50 my-7">
-            Hi! I'm Chathu Jayarathna, a <FlipWords words={flipewords} />
-          </h3>
+        <div className="p-4 max-w-7xl mx-auto relative z-10 w-full pt-44 md:pt-0">
+          {/* TextGenerateEffect for the tagline */}
+          <TextGenerateEffect
+            words="Transforming Ideas Into Dreams Through Technology"
+            className="text-center text-white text-[40px] md:text-5xl lg:text-6xl"
+          />
+          {/* Description text */}
+          <p className="text-center md:tracking-wider my-7 text-sm md:text-lg lg:text-2xl text-white">
+            Hi! I&apos;m Chathu, a Web Developer & Designer
+          </p>
+          
+          {/* MagicButton */}
+          <div className="flex justify-center items-center">
+            <a href="#about">
+              <MagicButton
+                title="Explore"
+                icon={<FaLocationArrow />}
+                position="right"
+              />
+            </a>
+          </div>
         </div>
       </div>
     </div>
   );
 }
- 
+
 export default Home
